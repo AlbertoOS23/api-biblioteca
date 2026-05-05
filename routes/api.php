b61ebfbcc2e8dc877e7499ccde14cd0b53af33a3
AlbertoOS23 <linkrojo13@gmail.com>
2026-05-05 20:48:20 +0200

    Add API routes and endpoints tested

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LibroController;
+
+Route::get('/libros', [LibroController::class, 'index']);
+Route::get('/libros/{id}', [LibroController::class, 'show']);
+Route::post('/libros', [LibroController::class, 'store']);
+Route::delete('/libros/{id}', [LibroController::class, 'destroy']);
