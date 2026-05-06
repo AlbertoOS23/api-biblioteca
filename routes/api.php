698022a048a6fc98801767f511aa54d6c46cb658
AlbertoOS23 <linkrojo13@gmail.com>
2026-05-06 18:58:16 +0200

    Add auth and protect private endpoints with Sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LibroController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/libros', [LibroController::class, 'index']);
 Route::get('/libros/{id}', [LibroController::class, 'show']);
-Route::post('/libros', [LibroController::class, 'store']);
-Route::delete('/libros/{id}', [LibroController::class, 'destroy']);
+Route::middleware('auth:sanctum')->group(function(){
+    Route::post('/libros', [LibroController::class, 'store']);
+    Route::delete('/libros/{id}', [LibroController::class, 'destroy']);
+});
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
